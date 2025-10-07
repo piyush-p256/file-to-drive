89c034ebb13be31fc14242d2ca51a6e1d92e6a3a
piyush-p256 <piyushpatel2562004@gmail.com>
2025-10-07 17:46:23 +0530

    Cors

--- fastapi_drive_upload.py
+++ fastapi_drive_upload.py
@@ -42,11 +42,7 @@ app = FastAPI()
 # ✅ Allow CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:8000",  # local Django
-        "http://localhost:8000",  # alternate local URL
-        "https://careercellbvcoe.in"  # (add when deployed)
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
